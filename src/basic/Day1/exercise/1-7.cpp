e7a455f43708764edefa65bc08b53da39c60cbef
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-05-14 21:52:04 +0900

    basic/prob.1-7

---
+++ src/basic/Day1/exercise/1-7.cpp
@@ -0,0 +1,18 @@
+// prob1-7(難易度：1)
+// 以下のプログラムはビルドエラーが発生し、実行できない。プログラムを修正し、期待される実行結果が得られるようにプログラムを直しなさい。
+/* -------------------
+#include <iostream>
+
+int main(){
+    cout << "HelloWorld." << endl;
+}
+------------------- */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "HelloWord." << endl;
+    return 0;
+}
