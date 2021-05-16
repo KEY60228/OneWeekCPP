d82e93545a5588c5e9665c85efae40153cf06899
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-05-16 13:29:48 +0900

    basic/prob.3-1

---
+++ src/basic/Day3/exercise/3-1/data.cpp
@@ -0,0 +1,32 @@
+/* -------------------
+#include "data.h"
+
+void CData::init()
+{
+    number = 0;
+    comment = "";
+}
+------------------- */
+
+#include "data.h"
+
+void CData::init() {
+    number = 0;
+    comment = "";
+}
+
+void CData::setNumber(int num) {
+    number = num;
+}
+
+int CData::getNumber() {
+    return number;
+}
+
+void CData::setComment(string str) {
+    comment = str;
+}
+
+string CData::getComment() {
+    return comment;
+}
